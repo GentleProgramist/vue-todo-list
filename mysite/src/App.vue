e5f4cd3052e87a3bef3f21a03e4eb55505d1f05c
DevStar234 <90933603+DevStar234@users.noreply.github.com>
2021-10-15 13:25:10 +0300

    Update App.vue

--- mysite/src/App.vue
+++ mysite/src/App.vue
@@ -3,14 +3,14 @@
     <h1>My To-Do List</h1>
     <to-do-form @todo-added="addToDo"></to-do-form>
     <h2 id="list-summary">{{listSummary}}</h2>
-    <ul aria-labelledby="list-summary" class="stack-large">
+    <ul aria-labelledby="list-summary" class="stack-large"> 
       <li v-for="item in ToDoItems" :key="item.id">
         <to-do-item :label="item.label" :done="item.done" :id="item.id"
             @checkbox-changed="updateDoneStatus(item.id)"
-            @item-deleted="deleteToDo(item.id)"
+            @item-deleted="deleteToDo(item.id)" 
             @item-edited="editToDo(item.id, $event)">
         </to-do-item>
-      </li> 
+      </li>
     </ul>
   </div>
 </template>
